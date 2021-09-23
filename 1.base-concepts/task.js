26a184aec88c078c1050d39c75201be5f7e5bcb5
ElizavetaChernis <elizaveta.chernysheva2@gmail.com>
2021-09-24 00:23:37 +0500

    task.js modified

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,15 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  "use strict";
+  let d = b**2 - 4 * a * c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b/(2*a)]
+  } else {
+    arr = [((-b + Math.sqrt(d) )/(2 * a)), ((-b - Math.sqrt(d) )/(2 * a))]
+  }
   return arr; // array
 }
 
@@ -8,6 +17,27 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+  "use strict";
+  if (isNaN(Number(percent))) {
+    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    return totalAmount;
+    }
+    if (isNaN(Number(contribution))) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+    }
+    if (isNaN(Number(amount))) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+    }
+    
+
+let credit = amount - contribution;
+let creditDate = new Date(date);
+let todayDate = new Date();
+let creditMonths = creditDate.getMonth() - todayDate.getMonth() + ((creditDate.getFullYear() - todayDate.getFullYear()) * 12);
+let interestRate = (percent / 12) / 100;
+let monthlyPay = credit * (interestRate + interestRate / (Math.pow((1 + interestRate), creditMonths) - 1));
+let totalAmountPay = monthlyPay * creditMonths;
+totalAmount = +totalAmountPay.toFixed(2)
 
   return totalAmount;
 }
