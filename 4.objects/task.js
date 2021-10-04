86c259d1e0443e9db66528be395283aab468a4db
ElizavetaChernis <elizaveta.chernysheva2@gmail.com>
2021-10-05 00:48:43 +0500

    task.js modified

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
     // Ваш код
-
+this.name = name;
+this.gander = gender;
+this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+this.subject = subjectName
 }
 
 // ваш код для остальных методов
+
+let student1 = new Student("Ирина", "женский", "25");
+let student2 = new Student("Александр", "мужской", "22");
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+  this.marks = [];
+  this.marks.push(mark);
+  } else {
+  this.marks.push(mark);
+  }
+  }
+
+Student.prototype.addMarks = function (...mark) {
+  this.marks = mark;
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((acc, item) => acc += item) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
